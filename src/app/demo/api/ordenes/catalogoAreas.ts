69c6a33ccaebc557735e95e1c740236f5a4e4b9e
lunaserrano <48137753+lunaserrano@users.noreply.github.com>
2025-03-24 00:04:03 -0600

    finalizacion creacion de areas y mesas

---
+++ src/app/demo/api/ordenes/catalogoAreas.ts
@@ -0,0 +1,5 @@
+export interface CatalogoAreasInterface {
+    cod_area: number;
+    nombre: string;
+    estado: string;
+}
--- src/app/demo/components/ordenes/ordenes.module.ts
+++ src/app/demo/components/ordenes/ordenes.module.ts
@@ -3,17 +3,74 @@ import { CommonModule } from '@angular/common';
 
 import { OrdenesRoutingModule } from './ordenes-routing.module';
 import { PedidosComponent } from './pedidos/pedidos.component';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
 import { TabViewModule } from 'primeng/tabview';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { CalendarModule } from "primeng/calendar";
+import { ChipsModule } from "primeng/chips";
+import { ChipModule } from "primeng/chip";
+import { InputMaskModule } from "primeng/inputmask";
+import { InputNumberModule } from "primeng/inputnumber";
+import { CascadeSelectModule } from "primeng/cascadeselect";
+import { MultiSelectModule } from "primeng/multiselect";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { InputTextModule } from "primeng/inputtext";
+import { RatingModule } from 'primeng/rating';
+import { KnobModule } from 'primeng/knob';
+import { ListboxModule } from 'primeng/listbox';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { SliderModule } from 'primeng/slider';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputDemoRoutingModule } from '../uikit/input/inputdemo-routing.module';
 
 @NgModule({
-  declarations: [
-     // Mueve PedidosComponent aquí
-  ],
+  declarations: [PedidosComponent],
   imports: [
     CommonModule,
     OrdenesRoutingModule,
-    PedidosComponent,
-    TabViewModule
+    TabViewModule,
+     ButtonModule, 
+     DialogModule, 
+     DropdownModule, 
+     FormsModule, 
+     ReactiveFormsModule,
+     CommonModule,
+     FormsModule,
+     InputDemoRoutingModule,
+     AutoCompleteModule,
+     CalendarModule,
+     ChipsModule,
+     DropdownModule,
+     InputMaskModule,
+     InputNumberModule,
+     ColorPickerModule,
+     CascadeSelectModule,
+     MultiSelectModule,
+     ToggleButtonModule,
+     SliderModule,
+     InputTextareaModule,
+     RadioButtonModule,
+     InputTextModule,
+     RatingModule,
+     ChipModule,
+     KnobModule,
+     InputSwitchModule,
+     ListboxModule,
+     SelectButtonModule,
+     CheckboxModule,
+     ButtonModule,
+     InputGroupModule,
+     InputGroupAddonModule
+    
   ]
 })
 export class OrdenesModule { }
